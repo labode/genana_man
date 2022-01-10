d3a5e0d7407c3700a157d54277197701334cc1b5
Jonas Labode <labode.jonas@mh-hannover.de>
2022-01-10 14:22:15 +0100

    Major workflow change: Due to problems caused by the usage of both node data (SGEXT) and edge date (genana.py) inconsistent data was generated. This is now mitigated by instead comparing two .nrrd files; .dot graph and .csv of vertex/generation are not used anymore. README.md was changed accordingly

--- genana_man.py
+++ genana_man.py
@@ -1,24 +1,5 @@
-import networkx as nx
-import sys
-import csv
+import argparse
 import nrrd
-
-
-def read_csv(file):
-    content = []
-
-    with open(file) as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        i = 0
-        for row in reader:
-            if i == 0:
-                i += 1
-                continue
-            else:
-                i += 1
-                content.append([row[0], row[1]])
-
-    return content
 
 
 def write_csv(data, target_file):
@@ -26,16 +7,10 @@ def write_csv(data, target_file):
     output_file = open(filename, "w")
 
     # Write header
-    output_file.write("Id; Vertex Id; Man_Gen\n")
+    output_file.write("Id; Man_Gen\n")
 
     for i in data:
-        output_file.write(str(i[0]) + '; ' + str(i[1]) + '; ' + str(i[2]) + '\n')
-
-
-def read_graph(file):
-    dot_graph = nx.drawing.nx_pydot.read_dot(file)
-
-    return dot_graph
+        output_file.write(str(i[0]) + '; ' + str(i[1]) + '\n')
 
 
 def read_nrrd(file):
@@ -44,39 +19,44 @@ def read_nrrd(file):
     return data
 
 
-def analyse(data, graph, nrrd_array):
-    analysis = []
+def analyze(nrrd_id, nrrd_gen):
+    results = []
 
-    for i in data:
-        coordinates = graph.node[str(i[0])]['spatial_node']
-        coordinates = coordinates.replace('"', '')
-        coordinates = coordinates.split(' ')
+    # Iterate through every array position
+    for i in range(len(nrrd_id)):
+        for j in range(len(nrrd_id[i])):
+            for k in range(len(nrrd_id[i][j])):
+                # Values of 0 (background label) are not of interest
+                if nrrd_id[i][j][k] != 0:
+                    # If we find a id/gen combination we do not know yet -> add to result list
+                    # Sometimes a node with a lower gen for the same id slips in (due to implicit node/edge conversion)
+                    # if it got into our list: We remove it, if we already have an entry with a higher gen, ignore it
+                    if [nrrd_id[i][j][k], nrrd_gen[i][j][k]] not in results \
+                            and [nrrd_id[i][j][k], int(nrrd_gen[i][j][k]) + 1] not in results:
+                        results.append([nrrd_id[i][j][k], nrrd_gen[i][j][k]])
+                    if [nrrd_id[i][j][k], int(nrrd_gen[i][j][k] - 1)] in results:
+                        results.remove([nrrd_id[i][j][k], int(nrrd_gen[i][j][k]) - 1])
 
-        id_label = nrrd_array[int(coordinates[0]), int(coordinates[1]), int(coordinates[2])]
-
-        result = [id_label, i[1]]
-        analysis.append(result)
-
-    return analysis
+    return results
 
 
 if __name__ == '__main__':
-    try:
-        genana_file = sys.argv[1]
-        dot_file = sys.argv[2]
-        nrrd_file = sys.argv[3]
-        output_csv = sys.argv[4]
+    parser = argparse.ArgumentParser(description='Create mapping from id to generation')
+    parser.add_argument('-i', '--id', action='store', type=str, required=True,
+                        help='.nrrd file containing ids')
+    parser.add_argument('-g', '--gen', action='store', type=str, required=True,
+                        help='.nrrd file containing generations')
+    parser.add_argument('-f', '--filename', action='store', type=str, required=True, help='Name of output file')
 
-    except IndexError:
-        sys.exit('Missing parameters \nPlease supply: generation file, dot graph, nrrd file,'
-                 ' output file name')
+    args = parser.parse_args()
 
-    genana = read_csv(genana_file)
-    nrrd_data = read_nrrd(nrrd_file)
-    graph_data = read_graph(dot_file)
+    nrrd_id_file_path = args.id
+    nrrd_gen_file_path = args.gen
+    output_filename = args.filename
 
-    results = analyse(genana, graph_data, nrrd_data)
-    write_csv(results, output_csv)
+    nrrd_id_file = read_nrrd(nrrd_id_file_path)
+    nrrd_gen_file = read_nrrd(nrrd_gen_file_path)
 
+    analysis = analyze(nrrd_id_file, nrrd_gen_file)
 
-
+    write_csv(analysis, output_filename)
